ed3f6c7d39a0698b6c5e730ec56976c6043da815
Jérémy Bouny <jbouny@gmail.com>
2015-06-03 15:58:14 +0200

    Externalize the morphing computing based on ancestors. Allows to take care of two levels instead of one (better morphing)

--- projected_grid_vs_lod/js/shaders/LODShader.js
+++ projected_grid_vs_lod/js/shaders/LODShader.js
@@ -25,6 +25,34 @@ THREE.ShaderChunk["lod_pars_vertex"] = [
     '              oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c );',
     '}',
     
+    'vec2 computeAncestorMorphing(int level, vec2 gridPosition, float heightMorphFactor, vec3 cameraScaledPosition, float resolution, vec2 previousMorphing )',
+    '{',
+      // Check if it is needed to apply the morphing (on 1 square on 2)
+    ' vec2 mantissa = gridPosition * resolution * 0.5;',
+    ' if( level > 1 ) {',
+    '   mantissa = ( mantissa + 0.5 ) / pow( 2.0, float( level - 1 ) );',
+    ' }',
+    ' mantissa -= floor( mantissa );',
+    
+      // Compute morphing factors (based on the height and the parent LOD
+    ' vec2 squareOffset = abs( cameraScaledPosition.xz - ( gridPosition + previousMorphing ) ) / float( level );',
+    ' vec2 comparePos = max( vec2( 0.0 ), squareOffset * 4.0 - 1.0 );',
+    ' float parentMorphFactor = min( 1.0, max( comparePos.x, comparePos.y ) );',
+    
+      // Compute the composition of morphing factors
+    ' vec2 morphFactor = vec2( 0.0 );',
+    ' if( mantissa.x + mantissa.y > 0.49 ) {',
+    '   float morphing = parentMorphFactor;',
+        // If first LOD, apply the height morphing factor everywhere
+    '   if( u_level + level == 1 ) {',
+    '     morphing = max( heightMorphFactor, morphing );',
+    '   }',
+    '   morphFactor += morphing * floor( mantissa * 2.0 );',
+    ' }',
+    ' return float( level ) * morphFactor / resolution;',
+      
+    '}',
+    
     'vec4 computePosition( vec4 position )',
     '{',
       // Extract the 3x3 rotation matrix and the translation vector from the 4x4 view matrix, then compute the camera position
@@ -35,6 +63,7 @@ THREE.ShaderChunk["lod_pars_vertex"] = [
     ' float resolution = float( u_resolution );',
     ' vec3 planeNormal = normalize( u_planeNormal );',
     
+    // Compute the plane rotation (if needed)
     ' mat3 planeRotation;',
     ' if( u_usePlaneParameters ) {',
     '   vec3 rotationAxis = cross( vec3( 0, 1.0, 0 ), planeNormal );',
@@ -42,7 +71,7 @@ THREE.ShaderChunk["lod_pars_vertex"] = [
     '   planeRotation = axisAngleToMatrix( rotationAxis, rotationAngle );',
     ' }',
     
-      // Project the camera position on the grid
+      // Project the camera position on the grid using plane parameters
     ' vec3 projectedCamera = vec3( cameraPosition.x, 0.0, cameraPosition.z );',
     ' if( u_usePlaneParameters ) {',
     '   projectedCamera = cameraPosition - dot( cameraPosition - ( planeNormal * u_planeDistance ), planeNormal ) * planeNormal;',
@@ -57,28 +86,19 @@ THREE.ShaderChunk["lod_pars_vertex"] = [
     ' }',
     ' vec2 gridPosition = position.xz + floor( cameraScaledPosition.xz * resolution + 0.5 ) / resolution;',
     
-      // Check if it is needed to apply the morphing (on 1 square on 2)
-    ' vec2 nextLevelMantissa = gridPosition * resolution * 0.5;',
-    ' nextLevelMantissa -= floor( nextLevelMantissa );',
-    
-      // Compute morphing factors (based on the height and the parent LOD
+      // Compute the height morphing factor
     ' float heightMorphFactor = cameraHeightLog - floor( cameraHeightLog );',
-    ' vec2 comparePos = max( vec2( 0.0 ), abs( ( cameraScaledPosition.xz - gridPosition ) * 4.0 ) - 1.0 ) ;',
-    ' float parentLODMorphFactor = max( comparePos.x, comparePos.y );',
     
-      // Compute the composition of morphing factors
-    ' vec2 morphFactor = vec2( 0.0 );',
-    ' if( nextLevelMantissa.x > 0.1 || nextLevelMantissa.y > 0.1 ) {',
-    '   float morphing = max( parentLODMorphFactor, heightMorphFactor * parentLODMorphFactor );',
-        // If first LOD, apply the height morphing factor everywhere
-    '   if( u_level == 0 ) {',
-    '     morphing = max( heightMorphFactor, parentLODMorphFactor );',
-    '   }',
-    '   morphFactor += morphing * min( vec2( 1.0 ), floor( nextLevelMantissa * 10.0 ) );',
+      // Compute morphing factors from LOD ancestors
+    ' vec2 morphing = vec2( 0 );',
+    ' for( int i = 1; i <= 2; ++i ) {',
+    '   morphing += computeAncestorMorphing( i, gridPosition, heightMorphFactor, cameraScaledPosition, resolution, morphing );',
     ' }',
     
-      // Apply the morphing
-    ' gridPosition += morphFactor / resolution;',
+      // Apply final morphing
+    ' gridPosition = gridPosition + morphing;',
+    
+      // Compute world coordinates (if needed)
     ' vec3 worldPosition = vec3( gridPosition.x * scale, 0, gridPosition.y * scale );',
     ' if( u_usePlaneParameters ) {',
     '   worldPosition.y += u_planeDistance;',
