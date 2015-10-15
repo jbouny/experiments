ee60c3aa26b87bab33534614e71170a4c9ebc6cf
Jérémy Bouny <jbouny@gmail.com>
2015-10-15 05:04:55 +0200

    Code formatting

--- pbr/js/demo.js
+++ pbr/js/demo.js
@@ -2,11 +2,11 @@
 
 THREE.ShaderLib[ 'cubemap' ] = {
 
-	uniforms: {
-		cubemap: { type: "t", value: null }
-	},
+  uniforms: {
+    cubemap: { type: "t", value: null }
+  },
 
-	vertexShader: [
+  vertexShader: [
     "varying vec3 vPosition;",
       
     "void main() {",
@@ -15,43 +15,43 @@ THREE.ShaderLib[ 'cubemap' ] = {
     "}",
   ].join( "\n" ),
 
-	fragmentShader: [
-		"uniform samplerCube cubemap;",
-		"varying vec3 vPosition;",
+  fragmentShader: [
+    "uniform samplerCube cubemap;",
+    "varying vec3 vPosition;",
     
-		"void main() ",
-		"{",
-			"gl_FragColor = textureCube(cubemap, normalize(vPosition));",
-		"}",
-	].join( "\n" )
+    "void main() ",
+    "{",
+      "gl_FragColor = textureCube(cubemap, normalize(vPosition));",
+    "}",
+  ].join( "\n" )
 
 };
 
 THREE.ShaderLib[ 'pbr' ] = {
 
-	uniforms: {
-		blur:      { type: "f", value: 0.1 },
-		sky:       { type: "t", value: null },
-		grunge:    { type: "t", value: null },
+  uniforms: {
+    blur:      { type: "f", value: 0.1 },
+    sky:       { type: "t", value: null },
+    grunge:    { type: "t", value: null },
     metallic:  { type: "f", value: 0.0 },
     baseColor: { type: "v3", value: new THREE.Vector3() },
     lightPos:  { type: "v3", value: new THREE.Vector3() },
-	},
+  },
 
-	vertexShader: [
+  vertexShader: [
     "varying vec3 vPosition;",
     "varying vec3 vNormal;",
     "varying vec3 vEye;",
     "varying vec2 vUv;",
 
     "vec3 getCameraPos()",
-		"{",
+    "{",
       "return - viewMatrix[3].xyz * mat3(",
         "viewMatrix[0].xyz,",
         "viewMatrix[1].xyz,",
         "viewMatrix[2].xyz",
       ");",
-		"}",
+    "}",
       
     "void main() {",
       "vPosition = position;",
@@ -63,31 +63,31 @@ THREE.ShaderLib[ 'pbr' ] = {
     "}",
   ].join( "\n" ),
 
-	fragmentShader: [
+  fragmentShader: [
 
-		"varying vec3 vPosition;",
+    "varying vec3 vPosition;",
     "varying vec3 vNormal;",
-		"varying vec3 vEye;",
+    "varying vec3 vEye;",
     "varying vec2 vUv;",
     
-		"uniform samplerCube sky;",
-		"uniform sampler2D grunge;",
-		"uniform vec3 lightPos;",
-		"uniform float metallic;",
-		"uniform vec3 baseColor;",
-		
+    "uniform samplerCube sky;",
+    "uniform sampler2D grunge;",
+    "uniform vec3 lightPos;",
+    "uniform float metallic;",
+    "uniform vec3 baseColor;",
+    
     // PBR implementation from Alexandre Pestana
     // www.alexandre-pestana.com
     "#define PI 3.14159265359",
 
-	  "vec3 Diffuse(vec3 pAlbedo)",
-		"{",
-		"    return pAlbedo/PI;",
-		"}",
-	  
+    "vec3 Diffuse(vec3 pAlbedo)",
+    "{",
+    "    return pAlbedo/PI;",
+    "}",
+    
     //-------------------------- Normal distribution functions --------------------------------------------
-		"float NormalDistribution_GGX(float a, float NdH)",
-		"{",
+    "float NormalDistribution_GGX(float a, float NdH)",
+    "{",
     "  // Isotropic ggx.",
     "  float a2 = a*a;",
     "  float NdH2 = NdH * NdH;",
@@ -95,52 +95,52 @@ THREE.ShaderLib[ 'pbr' ] = {
     "  denominator *= denominator;",
     "  denominator *= PI;",
     "  return a2 / denominator;",
-		"}",
+    "}",
 
-		//-------------------------- Geometric shadowing -------------------------------------------
-		"float Geometric_Smith_Schlick_GGX(float a, float NdV, float NdL)",
-		"{",
+    //-------------------------- Geometric shadowing -------------------------------------------
+    "float Geometric_Smith_Schlick_GGX(float a, float NdV, float NdL)",
+    "{",
     "  // Smith schlick-GGX.",
     "  float k = a * 0.5;",
     "  float GV = NdV / (NdV * (1.0 - k) + k);",
     "  float GL = NdL / (NdL * (1.0 - k) + k);",
     "  return GV * GL;",
-		"}",
+    "}",
 
-		//-------------------------- Fresnel ------------------------------------
-		"vec3 Fresnel_Schlick(vec3 specularColor, vec3 h, vec3 v)",
-		"{",
+    //-------------------------- Fresnel ------------------------------------
+    "vec3 Fresnel_Schlick(vec3 specularColor, vec3 h, vec3 v)",
+    "{",
     "  return (specularColor + (1.0 - specularColor) * pow((1.0 - clamp(dot(v, h), 0.0, 1.0)), 5.0));",
-		"}",
+    "}",
 
-		//-------------------------- BRDF terms ------------------------------------
-		"float Specular_D(float a, float NdH)",
-		"{",
-		"	return NormalDistribution_GGX(a, NdH);",
-		"}",
+    //-------------------------- BRDF terms ------------------------------------
+    "float Specular_D(float a, float NdH)",
+    "{",
+    "  return NormalDistribution_GGX(a, NdH);",
+    "}",
 
-		"vec3 Specular_F(vec3 specularColor, vec3 h, vec3 v)",
-		"{",
+    "vec3 Specular_F(vec3 specularColor, vec3 h, vec3 v)",
+    "{",
     "  return Fresnel_Schlick(specularColor, h, v);",
-		"}",
+    "}",
 
-		"vec3 Specular_F_Roughness(vec3 specularColor, float a, vec3 h, vec3 v)",
-		"{",
-		"	return (specularColor + (max(vec3(1.0 - a), specularColor) - specularColor) * pow((1.0 - clamp(dot(v, h), 0.0, 1.0)), 5.0));",
-		"}",
+    "vec3 Specular_F_Roughness(vec3 specularColor, float a, vec3 h, vec3 v)",
+    "{",
+    "  return (specularColor + (max(vec3(1.0 - a), specularColor) - specularColor) * pow((1.0 - clamp(dot(v, h), 0.0, 1.0)), 5.0));",
+    "}",
 
-		"float Specular_G(float a, float NdV, float NdL, float NdH, float VdH, float LdV)",
-		"{",
-		"	return Geometric_Smith_Schlick_GGX(a, NdV, NdL);",
-		"}",
+    "float Specular_G(float a, float NdV, float NdL, float NdH, float VdH, float LdV)",
+    "{",
+    "  return Geometric_Smith_Schlick_GGX(a, NdV, NdL);",
+    "}",
 
-		"vec3 Specular(vec3 specularColor, vec3 h, vec3 v, vec3 l, float a, float NdL, float NdV, float NdH, float VdH, float LdV)",
-		"{",
+    "vec3 Specular(vec3 specularColor, vec3 h, vec3 v, vec3 l, float a, float NdL, float NdV, float NdH, float VdH, float LdV)",
+    "{",
     "  return ((Specular_D(a, NdH) * Specular_G(a, NdV, NdL, NdH, VdH, LdV)) * Specular_F(specularColor, v, h) ) / (4.0 * NdL * NdV + 0.0001);",
-		"}",
+    "}",
 
-		"vec3 ComputeLight(vec3 albedoColor,vec3 specularColor, vec3 normal, vec3 lightPosition, vec3 lightColor, vec3 lightDir, vec3 viewDir, float roughness)",
-		"{",
+    "vec3 ComputeLight(vec3 albedoColor,vec3 specularColor, vec3 normal, vec3 lightPosition, vec3 lightColor, vec3 lightDir, vec3 viewDir, float roughness)",
+    "{",
     "  // Compute some useful values.",
     "  float NdL = clamp(dot(normal, lightDir), 0.0, 1.0);",
     "  float NdV = clamp(dot(normal, viewDir), 0.0, 1.0);",
@@ -152,10 +152,10 @@ THREE.ShaderLib[ 'pbr' ] = {
     "  vec3 cDiff = Diffuse(albedoColor);",
     "  vec3 cSpec = Specular(specularColor, h, viewDir, lightDir, a, NdL, NdV, NdH, VdH, LdV);",
     "  return lightColor * NdL * (cDiff * (1.0 - cSpec) + cSpec);",
-		"}",
+    "}",
 
-		"vec3 ComputeEnvColor(float roughness, vec3 reflectionVector)",
-		"{",
+    "vec3 ComputeEnvColor(float roughness, vec3 reflectionVector)",
+    "{",
       "vec3 reflect = normalize(reflect(vPosition - vEye, vNormal));",
       "vec3 left = cross(reflect, vec3(0.0, 1.0, 0.0));",
       "vec3 up = cross(reflect, left);",
@@ -172,10 +172,10 @@ THREE.ShaderLib[ 'pbr' ] = {
         "reflection += textureCube(sky, reflect).xyz;",
       "}",
       "return reflection;",
-		"}",
+    "}",
     
-		"void main() ",
-		"{",
+    "void main() ",
+    "{",
       "float roughness = texture2D(grunge, vUv).x;",
       "vec3 reflectVec = normalize(reflect(vPosition - vEye, vNormal));",
       
@@ -191,9 +191,9 @@ THREE.ShaderLib[ 'pbr' ] = {
       "vec3 envFresnel = Specular_F_Roughness(realSpecularColor, roughness * roughness, vNormal, viewDir);",
 
       "gl_FragColor = vec4(light1 + envFresnel * envColor + realAlbedo * 0.01, 1.0);",
-		"}",
+    "}",
 
-	].join( "\n" )
+  ].join( "\n" )
 
 };
 
